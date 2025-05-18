201ac646195f6d9cd7c136604cbf18c71637edc1
Igor Marlon <igormarlon.o@gmail.com>
2025-05-18 18:15:51 -0300

    feat(model): criar classe Tarefa e estrutura de pacotes MVC

--- src/model/Tarefa.java
+++ src/model/Tarefa.java
@@ -1,13 +1,71 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package model;
 
-/**
- *
- * @author igor.marlon
- */
+
 public class Tarefa {
+    private final int id;
+    private static int contadorId = 1;
+    private String titulo;
+    private String descricao;
+    private String prioridade;
+    private boolean concluida;
+    
+    //Método Construtor 
+    public Tarefa (String titulo, String descricao, String prioridade, boolean concluida){
+        this.id = contadorId++;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.prioridade = prioridade;
+        this.concluida = concluida;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    
+    public String getPrioridade(){
+        return prioridade;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    public void setPrioridade(String prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa{" + 
+                "id=" + id + 
+                ", titulo=" + titulo + '\'' +
+                ", descricao=" + descricao + '\'' +
+                ", prioridade=" + prioridade + '\'' +
+                ", concluida=" + concluida + '}';
+    }
+    
+    
+    
     
 }
